dd2e64a69b3c9081cfc91f17d2d6175400ac0b66
and-rs <deserd@protonmail.com>
2025-05-14 21:17:46 -0500

    updated statuscolumn plugin to use remote

--- .config/nvim/lua/plugins/statuscolumn.lua
+++ .config/nvim/lua/plugins/statuscolumn.lua
@@ -1,6 +1,5 @@
 return {
-  dir = "~/vault/dev/statuscolumn.nvim",
-  -- "JuanBaut/statuscolumn.nvim",
+  "JuanBaut/statuscolumn.nvim",
   config = function()
     require("statuscolumn").setup({
       enable_border = true,
