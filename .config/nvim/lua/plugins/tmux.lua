b2a25a477049918daf985f794c2b4077afb2d42a
and-rs <deserd@protonmail.com>
2025-09-22 10:37:34 -0500

    no tmux clipboard sync!

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -3,17 +3,7 @@ MiniDeps.now(function()
 
   require("tmux").setup({
     copy_sync = {
-      enable = true,
-      ignore_buffers = { empty = false },
-      redirect_to_clipboard = false,
-      register_offset = 0,
-
-      sync_registers_keymap_put = true,
-      sync_registers_keymap_reg = true,
-      sync_clipboard = true,
-      sync_registers = true,
-      sync_deletes = true,
-      sync_unnamed = true,
+      enable = false,
     },
     navigation = {
       enable_default_keybindings = false,
