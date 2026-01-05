23765ff59a731231e44eba2f27c45f5fa4231a30
and-rs <deserd@protonmail.com>
2026-01-04 22:21:38 -0500

    changes vscode-diff branch

--- lua/plugins/vscode-diff.lua
+++ lua/plugins/vscode-diff.lua
@@ -1,5 +1,9 @@
 MiniDeps.later(function()
-  MiniDeps.add({ source = "esmuellert/vscode-diff.nvim", dependencies = "MunifTanjim/nui.nvim" })
+  MiniDeps.add({
+    checkout = "next",
+    source = "esmuellert/vscode-diff.nvim",
+    dependencies = "MunifTanjim/nui.nvim",
+  })
   MiniDeps.add({ source = "MunifTanjim/nui.nvim" })
 
   vim.keymap.set("n", "<leader>gd", "<cmd>CodeDiff file HEAD<CR>", { desc = "Show Diff" })
